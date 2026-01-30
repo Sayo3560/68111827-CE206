ef729103bd8ca8bb0d17fadecc2f11344f346e7c
Sayo3560 <xuntun3560@gmail.com>
2026-01-30 16:57:10 +0700

    Add files via upload

---
+++ WorkShop3.1/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace NestedIf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("แบบฝึกหัดที่ 1: ตรวตสอบเลขบวก/ลบ และคู่/คี่");
+            Console.Write("ป้อนตัวเลข: ");
+            int value = int.Parse(Console.ReadLine());
+
+            if (value > 0)
+            {
+                Console.WriteLine("- เป็นจำนวนบวก");
+                if (value % 2 == 0)
+                {
+                    Console.WriteLine("- และเป็นเลขคู่");
+                }
+                else
+                {
+                    Console.WriteLine("- และเป็นเลขคี่");
+                }
+            }
+            else if (value < 0)
+            {
+                Console.WriteLine("- เป็นจำนวนลบ");
+                if (value % 2 == 0)
+                {
+                    Console.WriteLine("- และเป็นเลขคู่");
+                }
+                else
+                {
+                    Console.WriteLine("- และเป็นเลขคี่");
+                }
+            }
+            else
+            {
+                Console.WriteLine("- เป็นศูนย์");
+            }
+        }
+    }
+}
